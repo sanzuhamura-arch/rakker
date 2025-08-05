dfe0c258a263e1d80cbe03a072cea118eef6624c
ZarkDev098 <zarkdev09@gmail.com>
2025-08-06 00:12:51 +0800

    Update auto.js
    Signed-off-by: ZarkDev098 <zarkdev09@gmail.com>

--- script/event/auto.js
+++ script/event/auto.js
@@ -11,7 +11,7 @@ module.exports.config = {
 
 module.exports.handleEvent = async function ({ api, event }) {
   if (event.body) {
-    const emojis = ['', '', '', '', '', '', ''];
+    const emojis = ['💀', '🤪', '🤑', '🤮', '🥵', '🐸', '🤣', '🫵', '🥶,'];
     const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
     api.setMessageReaction(randomEmoji, event.messageID, () => {}, true);
